7ced8b66f588fe373ce886ccb5b87aa5285561ee
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-02 01:12:41 +0300

    comment

--- src/List.js
+++ src/List.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// props ile gönderdiğimiz veriyi alma 
 const List = ({people}) => {
   return (
     <>
+    {/* map ile data içerisinde gezdik destructuring yaptık */}
     {people.map((person) => {
       const {id, name, age, image} = person
+      // html tagleri içerisine yazdırma 
       return <article key={id} className="person">
         <img src={image} alt={name} />
         <div>
